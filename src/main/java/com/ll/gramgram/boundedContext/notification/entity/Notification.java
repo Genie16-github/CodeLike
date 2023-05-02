4a84f75e08915f3644fcfbd0975d0ddb8fbac926
Genie16-github <4411dlwoqja@naver.com>
2023-05-02 18:40:03 +0900

    design : 알림페이지 수정

--- src/main/java/com/ll/gramgram/boundedContext/notification/entity/Notification.java
+++ src/main/java/com/ll/gramgram/boundedContext/notification/entity/Notification.java
@@ -10,6 +10,7 @@ import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @Entity
 @Getter
@@ -36,5 +37,17 @@ public class Notification extends BaseEntity {
             case 2 -> "성격";
             default -> "능력";
         };
+    }
+
+    public String getAfterAddNotification() {
+        long diff = ChronoUnit.SECONDS.between(getCreateDate(), LocalDateTime.now());
+        if (diff < 60) return diff + "초";
+        else if (diff < 3600) {
+            return (diff / 60) + "분";
+        }
+        else if (diff < 86400) {
+            return (diff / 60 / 60) + "시간";
+        }
+        else return (diff / 60/ 60/ 24) + "일";
     }
 }
